4a43db4f40491b970fce4a6ca7c36aced94215dc
ManNV <mannv@vnext.com.vn>
2022-05-18 07:23:41 +0000

    #67 Change Japanese translation for special price period on product add and edit page

---
+++ app/code/Mpx/Marketplace/view/frontend/requirejs-config.js
@@ -0,0 +1,17 @@
+/**
+ * Mpx Software.
+ *
+ * @category  Mpx
+ * @package   Mpx_Marketplace
+ * @author    Mpx
+ */
+
+var config = {
+    config: {
+        mixins: {
+            'mage/validation': {
+                'Mpx_Marketplace/js/validation-mixins/japan-date-validation': true
+            }
+        }
+    }
+}
