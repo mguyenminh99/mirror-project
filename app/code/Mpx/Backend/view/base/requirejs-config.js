99a9a0ee5d0b0c6c53e978567d3bb266a5556614
manhnd2-vnext <manhnd2@vnext.com.vn>
2022-11-16 07:32:29 +0000

    #433 Translate marketplace manage seller Menu on admin page

---
+++ app/code/Mpx/Backend/view/base/requirejs-config.js
@@ -0,0 +1,15 @@
+/**
+ * Mpx Software.
+ *
+ * @category  Mpx
+ * @package   Mpx_Marketplace
+ * @author    Mpx
+ */
+
+var config = {
+    map: {
+        '*': {
+            'Magento_Ui/js/grid/filters/filters': 'Mpx_Backend/js/grid/filters/filters'
+        }
+    },
+}
