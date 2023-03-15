2b90c391f2633d0e966a056bdac4359b5b81fa2f
ManNV <mannv@vnext.com.vn>
2023-03-15 04:29:47 +0000

    #553 Add store limit setting validation

---
+++ app/code/Mpx/Marketplace/view/adminhtml/requirejs-config.js
@@ -0,0 +1,11 @@
+var config = {
+    config: {
+        mixins: {
+            'mage/validation': {
+                'Mpx_Marketplace/js/validation-mixins/validate-seller-range': true
+            },
+           
+        }
+    },
+   
+}
