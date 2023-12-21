fcea267ffcc3ccf9029755ae3b94b0e1c1c47a66
tanimoto@true-inc <tanimoto@true-inc.jp>
2023-12-21 12:23:52 +0900

    remove sql proxy

--- prod/docker/app/start.sh
+++ prod/docker/app/start.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-PROJECT_ID=$GOOGLE_CLOUD_PROJECT
-REGION=$GOOGLE_CLOUD_REGION
-INSTANCE_NAME=$SQL_INSTANCE_NAME
-
-INSTANCE_CONNECTION_NAME="$PROJECT_ID:$REGION:$INSTANCE_NAME"
-
-/var/www/html/prod/docker/app/cloud-sql-proxy $INSTANCE_CONNECTION_NAME &
-
 php /var/www/html/prod/docker/app/setup/init-script.php &
 
 exec apache2-foreground
